08f19cee64e4a43fa40b2a7ab3217dbfbbf47a66
jhm1643 <whgkaud@naver.com>
2019-07-18 13:53:59 +0900

    apns okhttp ok

--- JAiRO-PUSH/src/main/java/com/nexus/push/httpClient/HttpClient.java
+++ JAiRO-PUSH/src/main/java/com/nexus/push/httpClient/HttpClient.java
@@ -22,20 +22,24 @@ import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Connection;
 import okhttp3.ConnectionPool;
 import okhttp3.ConnectionSpec;
+import okhttp3.Handshake;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.Route;
 import okhttp3.TlsVersion;
 import okhttp3.internal.platform.ConscryptPlatform;
 import okio.Buffer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.Security;
 import java.security.cert.CertificateException;
@@ -67,6 +71,7 @@ import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.conscrypt.Conscrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Async;
@@ -223,41 +228,42 @@ public class HttpClient{
     	logger.info("PUSH HTTPclient DEVICE TYPE : {}",pushDomain.getDevice());
     	logger.info("PUSH HTTPclient DEVICE TOKEN : {}",pushDomain.getDevice_token());
     	logger.info("PUSH HTTPclient REQUEST URL : {}",pushDomain.getApns_full_url());
-    	final TrustManager[] trustAllCerts = new TrustManager[] {
-                new X509TrustManager() {
-                    @Override
-                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                    }
-
-                    @Override
-                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                    }
-
-                    @Override
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return new java.security.cert.X509Certificate[]{};
-                    }
-                }
-        };
-    	final SSLContext sslContext = SSLContext.getInstance("SSL");
-        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-
-        final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-    	List<Protocol> protocols = new ArrayList<Protocol>();
-    	protocols.add(Protocol.HTTP_2);
-    	protocols.add(Protocol.HTTP_1_1);
+//    	final TrustManager[] trustAllCerts = new TrustManager[] {
+//                new X509TrustManager() {
+//                    @Override
+//                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+//                    }
+//
+//                    @Override
+//                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+//                    }
+//
+//                    @Override
+//                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+//                        return new java.security.cert.X509Certificate[]{};
+//                    }
+//                }
+//        };
+//    	final SSLContext sslContext = SSLContext.getInstance("SSL");
+//        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+//        
+//        final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+//    	List<Protocol> protocols = new ArrayList<Protocol>();
+//    	protocols.add(Protocol.HTTP_2);
+//    	protocols.add(Protocol.HTTP_1_1);
+    	Security.insertProviderAt(Conscrypt.newProvider(), 1);
     	OkHttpClient client = new OkHttpClient.Builder()
     			//	.connectionSpecs(specs)
-    				.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0])
-    				.hostnameVerifier(new HostnameVerifier() {
-						
-						@Override
-						public boolean verify(String hostname, SSLSession session) {
-							// TODO Auto-generated method stub
-							return true;
-						}
-					})
-    				.protocols(protocols)
+    			//	.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0])
+//    				.hostnameVerifier(new HostnameVerifier() {
+//						
+//						@Override
+//						public boolean verify(String hostname, SSLSession session) {
+//							// TODO Auto-generated method stub
+//							return true;
+//						}
+//					})
+    			//	.protocols(protocols)
 					.connectTimeout(60, TimeUnit.MINUTES)
 					.retryOnConnectionFailure(true)
 					.connectionPool(new ConnectionPool(5, 1, TimeUnit.SECONDS))
@@ -268,17 +274,21 @@ public class HttpClient{
 		
 		Request request = null;
 		try {
-			logger.info("시작");	
-			long afterTime = 0;
-			long secDiffTime = 0;
-			long beforeTime = System.currentTimeMillis();
+			logger.info("요청 시작");	
+			long request_end_time = 0;
+			long response_end_time = 0;
+			long request_exec_time = 0;
+			long response_exec_time = 0;
+			long request_start_time = System.currentTimeMillis();
 			//argument 값에 사용자 count를 넣으면 됨
-			CountDownLatch cdl = new CountDownLatch(1);
-			for(int i=0;i<1;i++) {
+			CountDownLatch cdl = new CountDownLatch(3000);
+			for(int i=0;i<3000;i++) {
 				request = new Request.Builder()
 						.addHeader("Authorization", "Bearer " + pushDomain.getServer_token())
+						.addHeader("apns-topic", pushDomain.getApns_topic())
 		        		.url(pushDomain.getApns_full_url())
 		        		.post(RequestBody.create(pushDomain.getPost_data(),JSON))
+		        		
 		        		.build();
 				client.newCall(request).enqueue(new Callback() {
 					
@@ -286,12 +296,15 @@ public class HttpClient{
 					public void onResponse(Call call, Response response) throws IOException {
 						// TODO Auto-generated method stub
 						
-					//	logger.info("carrey : "+response.code());
-						Buffer buffer = new Buffer();
-						call.request().body().writeTo(buffer);
-						
-					//	logger.info("성공 : "+buffer.readUtf8());
-					//	map.put("success", map.get("success")+1);	
+//						Buffer buffer = new Buffer();
+//						call.request().body().writeTo(buffer);
+//						if(response.code()==200) {
+//							//logger.info("성공 : "+buffer.readUtf8());
+//							map.put("success", map.get("success")+1);	
+//							success++;
+//						}else {
+//							logger.info("fail : "+response.code());
+//						}
 						success++;
 						cdl.countDown();
 						response.close();
@@ -304,7 +317,7 @@ public class HttpClient{
 						try {
 							Buffer buffer = new Buffer();
 							call.request().body().writeTo(buffer);
-							logger.info("실패 : "+buffer.readUtf8());
+							//logger.info("실패 : "+buffer.readUtf8());
 						}catch(IOException e1) {
 							e1.printStackTrace();
 						}finally {
@@ -315,11 +328,14 @@ public class HttpClient{
 					}
 				});
 			}
-			logger.info("종료");
-			logger.info("요청 시간 : "+secDiffTime);
-			afterTime = System.currentTimeMillis(); 
-			secDiffTime = (afterTime - beforeTime)/1000;
+			request_end_time = System.currentTimeMillis(); 
+			request_exec_time = (request_end_time - request_start_time)/1000;
+			logger.info("요청 종료");
+			logger.info("요청 처리 시간 : "+request_exec_time);
 			cdl.await();
+			response_end_time = System.currentTimeMillis();
+			response_exec_time = (response_end_time - request_start_time)/1000;
+			logger.info("응답 완료 시간 : "+response_exec_time);
 			logger.info("성공 개수 : "+success);
 			success=0;
 //			for(String key:map.keySet()) {
