44cc02fc6d557417c4cdf98ea718c8f9dafe4299
jhm1643 <40678477+jhm1643@users.noreply.github.com>
2022-05-22 16:38:54 +0900

    Delete ucex directory

--- ucex/src/main/java/api/domain/TERMINAL_TYPE.java
+++
@@ -1,9 +0,0 @@
-package api.domain;
-
-public class TERMINAL_TYPE {
-	public static final String UCE_PC_CLIENT = "2341";
-	public static final String UCE_ANDROID_CLIENT = "2342";
-	public static final String UCE_IOS_CLIENT = "2343";
-	public static final String UCE_ANDROID_TABLET = "2344";
-	public static final String UCE_IOS_TABLET = "2345";
-}
